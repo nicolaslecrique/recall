48784307dfc3fb5f707e56e754daaf70caae4e59
Nicolas <nicolas.lecrique+git@globers.co>
2023-01-05 17:13:15 +0100

    add firebase dep

--- recall_app/lib/models/user_model.dart
+++ recall_app/lib/models/user_model.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import '../firebase_options.dart';
+
 class UserModel extends ChangeNotifier {
-  Future<void> init() async {}
+  Future<void> init() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   List<String> getAll() {
     return List.of(["aa", "bb", "cc"]);
