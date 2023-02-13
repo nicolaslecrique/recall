7db91bb1508f2635e1b794e2556d48944b4f1cfd
Nicolas <nicolas.lecrique+git@globers.co>
2023-02-13 23:42:42 +0100

    move rest api from main to api/api.py
    following
    https://fastapi.tiangolo.com/tutorial/bigger-applications/

--- recall_back/api/api.py
+++ recall_back/api/api.py
@@ -1,1 +1,32 @@
+from typing import Dict
 
+from fastapi import APIRouter, Header
+from firebase_admin import auth
+
+from api.api_converters import to_api_user_data_snapshot
+from api.api_model import ApiUserDataSnapshot
+from model.crud_service import CrudService
+from model.model import UserData
+
+router = APIRouter()
+crud_service: CrudService = CrudService()
+
+@router.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "Hello World"}
+
+
+@router.get("/user")
+async def get_or_create(authorization: str = Header()) -> ApiUserDataSnapshot:
+    id_token: str = authorization[7:]  # remove "Bearer " prefix
+    decoded_token: Dict[str, str] = auth.verify_id_token(id_token)
+    uid: str = decoded_token["uid"]
+    email: str = decoded_token["email"]
+    user_data: UserData = crud_service.get_or_create_user(uid, email)
+    snapshot: ApiUserDataSnapshot = to_api_user_data_snapshot(user_data)
+    return snapshot
+
+
+@router.put("/item")
+async def create_or_update_item(authorization: str = Header()):
+    pass
--- recall_back/main.py
+++ recall_back/main.py
@@ -1,37 +1,12 @@
-from tracemalloc import Snapshot
-from typing import Dict
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
 import firebase_admin
-from firebase_admin import credentials, auth
-from api.api_converters import to_api_user_data_snapshot
+from firebase_admin import credentials
+from api import api
 
-from api.api_model import ApiUserDataSnapshot
-from model.crud_service import CrudService
-from model.model import UserData
-from tools.uri_utils import generate_uri
 
 app = FastAPI()
 cred = credentials.Certificate("secrets/firebase_service_account_key.json")
 firebase_admin.initialize_app(cred)
-crud_service: CrudService = CrudService()
 
+app.include_router(api.router)
 
-@app.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Hello World"}
-
-
-@app.get("/user")
-async def get_or_create(authorization: str = Header()) -> ApiUserDataSnapshot:
-    id_token: str = authorization[7:]  # remove "Bearer " prefix
-    decoded_token: Dict[str, str] = auth.verify_id_token(id_token)
-    uid: str = decoded_token["uid"]
-    email: str = decoded_token["email"]
-    user_data: UserData = crud_service.get_or_create_user(uid, email)
-    snapshot: ApiUserDataSnapshot = to_api_user_data_snapshot(user_data)
-    return snapshot
-
-
-@app.put("/item")
-async def create_or_update_item(authorization: str = Header()):
-    pass
