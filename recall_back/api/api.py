970ecdeafc4d62f246ef60bcc5468cb9cd547775
Nicolas <nicolas.lecrique+git@globers.co>
2023-02-14 22:28:00 +0100

    refactor api to use Depends
    (to get user uid in each method requiriqing authentication

--- recall_back/api/api.py
+++ recall_back/api/api.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
 from typing import Dict
 
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Depends, Header
 from firebase_admin import auth
 
 from api.api_converters import to_api_user_data_snapshot
@@ -8,25 +9,32 @@ from api.api_model import ApiUserDataSnapshot
 from model.crud_service import CrudService
 from model.model import UserData
 
-router = APIRouter()
+router: APIRouter = APIRouter()
 crud_service: CrudService = CrudService()
 
 @router.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
+@dataclass
+class UserIdentity:
+    uid: str
+    email: str
 
-@router.get("/user")
-async def get_or_create(authorization: str = Header()) -> ApiUserDataSnapshot:
+
+async def get_identity(authorization: str = Header()) -> UserIdentity:
     id_token: str = authorization[7:]  # remove "Bearer " prefix
     decoded_token: Dict[str, str] = auth.verify_id_token(id_token)
-    uid: str = decoded_token["uid"]
-    email: str = decoded_token["email"]
-    user_data: UserData = crud_service.get_or_create_user(uid, email)
+    return UserIdentity(decoded_token["uid"], decoded_token["email"])
+
+
+@router.get("/user")
+async def get_or_create(identity: UserIdentity = Depends(get_identity)) -> ApiUserDataSnapshot:
+    user_data: UserData = crud_service.get_or_create_user(identity.uid, identity.email)
     snapshot: ApiUserDataSnapshot = to_api_user_data_snapshot(user_data)
     return snapshot
 
 
 @router.put("/item")
-async def create_or_update_item(authorization: str = Header()):
+async def create_or_update_item(identity: UserIdentity = Depends(get_identity)):
     pass
